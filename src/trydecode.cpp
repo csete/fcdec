8c51bcfd27c773a729702f0564a3c66c27880bdd
Alexandru Csete <oz9aec@gmail.com>
2013-11-29 00:27:43 +0100

    Switch to reading raw input from stdin.

--- src/trydecode.cpp
+++ src/trydecode.cpp
@@ -1,11 +1,11 @@
 // FUNcube decoder test program
 // Copyright (c) Phil Ashby, 2013
+// Copyright (c) Alexandru Csete, 2013
 // Released under the terms of the Creative Commons BY-SA-NC
 // https://creativecommons.org/licenses/by-nc-sa/3.0/
 
 #include <stdio.h>
 #include <unistd.h>
-#include <sndfile.h>
 #include <sys/time.h>
 #include <math.h>
 #include "stdafx.h"
@@ -59,74 +59,72 @@ CTryDecode::~CTryDecode(void) {
 }
 
 void CTryDecode::go(void) {
-	// Grab audio samples from test.wav and convert to complex values (via libsndfile),
-	// apply forward FFT, deduce centre frequency then feed to decoder at sane rate.
-	SF_INFO sfi = { 0 };
-	sf_count_t n;
-	SNDFILE *sf = sf_open("test.wav", SFM_READ, &sfi);
-	if (!sf) {
-		perror("opening test.wav");
-		return;
-	}
-	if (sfi.samplerate != PA_SAMPLE_RATE || sfi.channels != 2) {
-		fprintf(stderr, "Unexpected format (not 96kHz 2 channel): 0x%08X\n", sfi.format);
-		sf_close(sf);	
-		return;
-	}
-	sf_command(sf, SFC_SET_NORM_FLOAT, NULL, SF_FALSE);
+	// Grab audio samples from stdin in S16_LE format apply forward FFT,
+    // deduce centre frequency then feed to decoder at sane rate.
+    size_t n;
+
 	do {
-		short buf[2*FORWARD_FFT_SIZE];
+
+		short  buf[2*FORWARD_FFT_SIZE];
 		struct timeval ti;
 		struct timeval te;
-		float maxBin=0.0;
-		int binPos = -1;
-		gettimeofday(&ti, NULL);
-		n = sf_read_short(sf, buf, 2*FORWARD_FFT_SIZE);
-		if (n<=0)
+		float  maxBin=0.0;
+		int    binPos = -1;
+
+        gettimeofday(&ti, NULL);
+		n = fread(buf, sizeof(short), 2*FORWARD_FFT_SIZE, stdin);
+
+        if (n < 2*FORWARD_FFT_SIZE)
 			continue;
-		for(int i=0; i<FORWARD_FFT_SIZE; i++) {
-			_fftIn[i][0] = (float)(buf[2*i]+44)/32768.0;
-			_fftIn[i][1] = (float)(buf[2*i+1]-4)/32768.0;
+
+        for (int i = 0; i < FORWARD_FFT_SIZE; i++)
+        {
+			_fftIn[i][0] = (float)(buf[2*i]/*+44*/)/32768.0;
+			_fftIn[i][1] = (float)(buf[2*i+1]/*-4*/)/32768.0;
 		}
 		fftwf_execute(_fftPlan);
 		_avePsd[0]=0;
-		// NB: FORWARD_FFT_RESULT_SIZE = FORWARD_FFT_SIZE/2 since we ignore negative frequencies in results
-		for(int i=0;i<FORWARD_FFT_RESULT_SIZE;i++)
+
+        // NB: FORWARD_FFT_RESULT_SIZE = FORWARD_FFT_SIZE/2 since we ignore negative frequencies in results
+		for (int i = 0; i < FORWARD_FFT_RESULT_SIZE; i++)
 		{
 			_psd[i] = sqrt(_fftOut[i][0]*_fftOut[i][0]+_fftOut[i][1]*_fftOut[i][1]);
 		}
 		int beg = _doupper ? FORWARD_FFT_RESULT_SIZE/2 : 0;
 		int end = _doupper ? FORWARD_FFT_RESULT_SIZE : FORWARD_FFT_RESULT_SIZE/2;
-		for(int i=beg+75;i<end-75;i++)
+		for (int i = beg+75; i < end-75; i++)
 		{	
 			_avePsd[i]=0;
-			for(int j=i-50;j<i+50;j++)
+			for (int j = i-50; j < i+50; j++)
 			{
 				_avePsd[i] += _psd[j];
 			}		
 	
-			if(maxBin<_avePsd[i])
+			if (maxBin < _avePsd[i])
 			{
 				maxBin = _avePsd[i];
 				binPos = i;
 			}
 		}
-		_centreBin=max(0,min(FORWARD_FFT_RESULT_SIZE-1,_centreBin));
-		_averagePeekPower=(PSD_AVERAGE_FACTOR * _avePsd[_centreBin]) + (PSD_INV_AVERAGE_FACTOR * _averagePeekPower);
-		if(maxBin>(_averagePeekPower/4)*5 && 0<binPos)
+		_centreBin = max(0, min(FORWARD_FFT_RESULT_SIZE-1, _centreBin));
+		_averagePeekPower = (PSD_AVERAGE_FACTOR * _avePsd[_centreBin]) + (PSD_INV_AVERAGE_FACTOR * _averagePeekPower);
+		if (maxBin > (_averagePeekPower/4)*5 && 0 < binPos)
 		{
 			_averageCentreBin = (CFREQ_AVERAGE_FACTOR * (float)binPos) + (CFREQ_INV_AVERAGE_FACTOR * _averageCentreBin);
-			_centreBin = (int)_averageCentreBin+1.0F;
+			_centreBin = (int) _averageCentreBin + 1.0F;
 		}
-		_decoder->ProcessFFT(_fftOut, _centreBin);
-		gettimeofday(&te, NULL);
-		int period=(te.tv_sec-ti.tv_sec)*1000000+(te.tv_usec-ti.tv_usec);
-		int us=PROCESSING_PERIOD_USEC-period;
-		fprintf(stderr, "processing took: %d usecs, centreBin=%d\n", period, _centreBin);
-		if (_dosleep)
+
+        _decoder->ProcessFFT(_fftOut, _centreBin);
+
+        gettimeofday(&te, NULL);
+		int period = (te.tv_sec - ti.tv_sec) * 1000000 + (te.tv_usec - ti.tv_usec);
+		int us = PROCESSING_PERIOD_USEC - period;
+
+        fprintf(stderr, "processing took: %d usecs, centreBin=%d\n", period, _centreBin);
+
+        if (_dosleep)
 			usleep(us);
-	} while (n>0);
-	sf_close(sf);
+	} while (n > 0);
 }
 
 void CTryDecode::OnDecodeSuccess(U8* result, U32 length, U32 centreBin) {
