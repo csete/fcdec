d8bc02a47b5c98a3f6c4fc4863b6dbeeca41d6a2
Alexandru Csete <oz9aec@gmail.com>
2013-12-24 18:56:27 +0100

    Clean-up.

--- filter/main.cpp
+++ filter/main.cpp
@@ -28,7 +28,7 @@
 //
 // The views and conclusions contained in the software and documentation are
 // those of the authors and should not be interpreted as representing official
-// policies, either expressed or implied, of Moe Wheatley.
+// policies, either expressed or implied, of Alexandru Csete.
 //
 #include <stdlib.h>
 #include <stdint.h>
